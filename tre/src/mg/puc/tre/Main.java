c4ca7d418f82ddc81bd8ed92ca4d2bdccd828caf
Charles Fonseca <charlesjrfonseca@gmail.com>
2020-05-23 16:13:56 -0300

    ReadFile

--- tre/src/mg/puc/tre/Main.java
+++ tre/src/mg/puc/tre/Main.java
@@ -1,8 +1,16 @@
 package mg.puc.tre;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
 
   public static void main(String[] args) {
     // write your code here
+  }
+
+  public static String readFile(String path) throws IOException {
+    return Files.readString(Paths.get(path));
   }
 }
